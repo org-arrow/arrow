adf64262ef11be4def08ea22e4d5cd9d221d6650
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-04-20 00:20:09 +0300

    feat: ability to have more than 1 subscription

--- contracts/Subscriptions.sol
+++ contracts/Subscriptions.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+/// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -17,54 +17,64 @@ contract Subscription is Ownable {
     address[] public subscribers;
     address[] public failedTransactions;
 
-    mapping(address => SubscriptionDetails) public subscriptions;
+    mapping(address => SubscriptionDetails[]) public subscriptions;
 
     constructor(IERC20 _token) Ownable(msg.sender) {
         token = _token;
     }
 
-    function subscribe(uint256 _serviceId, uint256 _amount, uint256 _duration) external {
+    function subscribe(
+        uint256 _serviceId,
+        uint256 _amount,
+        uint256 _duration
+    ) external {
         require(_amount > 0, "subscribe::amount must be greater than 0");
         require(_duration > 0, "subscribe::duration must be greater than 0");
 
         token.approve(address(this), type(uint256).max);
 
-        subscriptions[msg.sender] = SubscriptionDetails(
-            _serviceId,
-            _amount,
-            _duration,
-            block.timestamp
+        subscriptions[msg.sender].push(
+            SubscriptionDetails(_serviceId, _amount, _duration, block.timestamp)
         );
         subscribers.push(msg.sender);
     }
 
-    function unsubscribe() external {
-        delete subscriptions[msg.sender];
-        for (uint256 i = 0; i < subscribers.length; i++) {
-            if (subscribers[i] == msg.sender) {
-                subscribers[i] = subscribers[subscribers.length - 1];
-                subscribers.pop();
+    function unsubscribe(uint256 _serviceId) external {
+        SubscriptionDetails[] storage userSubscriptions = subscriptions[
+            msg.sender
+        ];
+        for (uint256 i = 0; i < userSubscriptions.length; i++) {
+            if (userSubscriptions[i].serviceId == _serviceId) {
+                userSubscriptions[i] = userSubscriptions[
+                    userSubscriptions.length - 1
+                ];
+                userSubscriptions.pop();
                 break;
             }
         }
     }
 
-    function collect() external onlyOwner {
+    function collect(uint256 _serviceId) external onlyOwner {
         for (uint256 i = 0; i < subscribers.length; i++) {
             address subscriber = subscribers[i];
-            SubscriptionDetails memory details = subscriptions[subscriber];
-            if (details.lastPaidTime + details.duration <= block.timestamp) {
-                bool success = token.transferFrom(
-                    subscriber,
-                    msg.sender,
-                    details.amount
-                );
-                if (success) {
-                    details.lastPaidTime = block.timestamp;
-                    subscriptions[subscriber] = details;
-                    _removeFailedTransaction(subscriber);
-                } else {
-                    failedTransactions.push(subscriber);
+            SubscriptionDetails[] storage details = subscriptions[subscriber];
+            for (uint256 j = 0; j < details.length; j++) {
+                if (
+                    details[j].serviceId == _serviceId &&
+                    details[j].lastPaidTime + details[j].duration <=
+                    block.timestamp
+                ) {
+                    bool success = token.transferFrom(
+                        subscriber,
+                        msg.sender,
+                        details[j].amount
+                    );
+                    if (success) {
+                        details[j].lastPaidTime = block.timestamp;
+                        _removeFailedTransaction(subscriber);
+                    } else {
+                        failedTransactions.push(subscriber);
+                    }
                 }
             }
         }
